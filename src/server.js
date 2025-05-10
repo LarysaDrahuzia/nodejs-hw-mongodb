6e473e19b1b9e7aafc4aeac77753e499bad2da67
Larysa <draglara@ukr.net>
2025-05-10 10:36:16 +0300

    fixed

--- src/server.js
+++ src/server.js
@@ -28,7 +28,8 @@ export const setupServer = () => {
 
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
-    res.status(200).json({
+    res.json({
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
@@ -44,7 +45,8 @@ export const setupServer = () => {
       });
       return;
     }
-    res.status(200).json({
+    res.json({
+      status: 200,
       message: `Successfully found contact with id: ${contactId}!`,
       data: contact,
     });
