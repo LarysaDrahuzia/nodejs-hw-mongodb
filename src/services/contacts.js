39819dd5178855d374c0edd0b3fba1a61452d371
Larysa <draglara@ukr.net>
2025-05-31 04:50:26 +0300

    fixed services/contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -41,7 +41,7 @@ export const getAllContacts = async ({
 };
 
 export const getContactId = async ({ _id, userId }) => {
-  const contact = await ContactsCollection.findById({ _id, userId });
+  const contact = await ContactsCollection.findOne({ _id, userId });
   return contact;
 };
 
